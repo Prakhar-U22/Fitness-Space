1ab1053a34b97239a4240bc75f36840fe9bae364
Vishal <vishal.student.eee22@itbhu.ac.in>
2025-12-04 18:08:41 +0530

    new Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Body from "./Body";
 import "./Home.css";
 
 const Home = () => {
   return (
     <div className="home-wrapper">
       <Navbar />
-
+      
       {/* Empty main area for now */}
       <main className="home-main">
-        {/* You can add your sections here later */}
+        <Body/>  {/* You can add your sections here later */}
       </main>
     </div>
   );
