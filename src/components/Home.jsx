3cb977f2f01231b0daee7902ffa0f78bb5224804
Vishal <vishal.student.eee22@itbhu.ac.in>
2025-12-04 17:50:30 +0530

    Home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./Navbar";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home-wrapper">
+      <Navbar />
+
+      {/* Empty main area for now */}
+      <main className="home-main">
+        {/* You can add your sections here later */}
+      </main>
+    </div>
+  );
+};
+
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,12 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="left">
-        <button className="btn home-btn">Home</button>
+      <div className="nav-left">
+        <button className="nav-btn home-btn">Home</button>
       </div>
 
-      <div className="right">
-        <button className="btn login-btn">Login</button>
+      <div className="nav-right">
+        <button className="nav-btn login-btn">Login</button>
       </div>
     </nav>
   );
